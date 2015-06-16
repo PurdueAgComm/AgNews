aab10500db3577262e4d11073d393f49d6f5dd31
Kenny Wilson <kenny@digital-inflection.com>
2015-06-16 15:41:16 -0400

    display archive back to 2010

--- notables/archives.php
+++ notables/archives.php
@@ -129,35 +129,28 @@ google.setOnLoadCallback(function() {
   <div class="container">
     <div class="row">
       <div class="maincontent col-lg-9 col-md-9 col-sm-9 left">
-
-
-<h1>Archives</h1>
-<p> Cras sed orci pellentesque, porttitor odio nec, convallis odio. Maecenas id tellus enim. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
-
-<h2>2015</h2>
-    <ul>
-          <a><li>uis vitae lacinia dui, ac lacinia dolor.</li></a>
-          <a><li>uis vitae lacinia dui, ac lacinia dolor.</li></a>
-          <a><li>uis vitae lacinia dui, ac lacinia dolor.</li></a>
-          <a><li>uis vitae lacinia dui, ac lacinia dolor.</li></a>
-    </ul>
-
-<h2>2014</h2>
-    <ul>
-          <a><li>uis vitae lacinia dui, ac lacinia dolor.</li></a>
-          <a><li>uis vitae lacinia dui, ac lacinia dolor.</li></a>
-          <a><li>uis vitae lacinia dui, ac lacinia dolor.</li></a>
-          <a><li>uis vitae lacinia dui, ac lacinia dolor.</li></a>
-    </ul>
-
-<h2>2013</h2>
-    <ul>
-          <a><li>uis vitae lacinia dui, ac lacinia dolor.</li></a>
-          <a><li>uis vitae lacinia dui, ac lacinia dolor.</li></a>
-          <a><li>uis vitae lacinia dui, ac lacinia dolor.</li></a>
-          <a><li>uis vitae lacinia dui, ac lacinia dolor.</li></a>
-    </ul>
-
+        <h1>Archives</h1>
+        <p> Cras sed orci pellentesque, porttitor odio nec, convallis odio. Maecenas id tellus enim. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+        <?php
+          $year = date("Y");
+          // no stories beyond 2010
+          while($year >= 2010) {
+            $sql = "SELECT tblNews.strHeadline, tblNews.newsID FROM tblNews LEFT JOIN tblNewsArea ON tblNews.newsID=tblNewsArea.newsID WHERE tblNews.stageID=6 AND tblNewsArea.areaID=5 AND tblNews.isHidden=0 AND YEAR(tblNews.datePublished)=" . $year . " ORDER BY datePublished DESC";
+            $result = mysql_query($sql);
+            $numResult = mysql_num_rows($result);
+            if($numResult > 0) {
+              echo "<h2>" . $year . "</h2>";
+              echo "<ul>";
+              while($notable = mysql_fetch_array($result)) {
+                echo "<li>";
+                echo "<a href='notable.php?n=" . $notable["newsID"] . "'>" . $notable["strHeadline"] . "</a>";
+                echo "</li>";
+              }
+              echo "</ul>";
+            } // end if
+            $year--;
+          } //end year while
+          ?>
       </div>
 
       <div class="sidenav col-lg-3 col-md-3 col-sm-3">
@@ -165,7 +158,7 @@ google.setOnLoadCallback(function() {
         <ul>
           <?php
           $sql = "SELECT * FROM tblNews LEFT JOIN tblNewsArea ON tblNews.newsID=tblNewsArea.newsID WHERE tblNews.stageID=6 AND tblNewsArea.areaID=5 AND tblNews.isHidden=0 ORDER BY tblNews.datePublished DESC LIMIT 10";
-          $result = mysql_query($sql) or die(mysql_error());
+          $result = mysql_query($sql);
           while($notable = mysql_fetch_array($result)) : ?>
             <li><a href="notable.php?n=<?php echo $notable["newsID"]?>"><?php echo $notable["strHeadline"]; ?></a></li>
         <?php endwhile; ?>
