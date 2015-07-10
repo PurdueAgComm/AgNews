5f7c4f94c3a7b19586020b1b34bf42ecb30e0359
Kenny Wilson <kenny@digital-inflection.com>
2015-07-10 17:11:37 -0400

    UI updates

--- includes/header.php
+++ includes/header.php
@@ -1,16 +1,13 @@
 
 <!DOCTYPE html>
-
 <?php
 // global includes
 include_once('includes/auth.php'); // authenticate users, includes db connection
-
 // how many stories are waiting on them?
 $redSQL = "SELECT * FROM tblNews INNER JOIN tblNewsWriterPeople ON tblNews.newsID = tblNewsWriterPeople.newsID WHERE tblNews.isHidden=0 AND tblNews.statusID=1 AND tblNewsWriterPeople.peopleID ='" . $_SESSION["userID"] . "';";
 $redResult = mysql_query($redSQL);
 $_SESSION["waitingOnWriter"] = mysql_num_rows($redResult);
 ?>
-
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -18,52 +15,43 @@ $_SESSION["waitingOnWriter"] = mysql_num_rows($redResult);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet"/>
-
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="js/html5shiv.js"></script>
     <![endif]-->
-
   </head>
-
   <body>
-
   <div class="container">
-
-      <!-- Static navbar -->
-      <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="/agnewsdb/index.php">AgNews DB</a>
-          </div>
-          <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-                <li><a href="/agnewsdb/index.php"><span data-container="body" data-placement="bottom" rel='tooltip' title='You have <?php echo  $_SESSION["waitingOnWriter"];?> action(s) waiting' class='badge badge-info'><?php if($_SESSION["waitingOnWriter"] != 0) { echo $_SESSION["waitingOnWriter"]; } ?></span></a></li>
-                <li><a href="viewAllStories.php"><i class="fa fa-newspaper-o"></i> News Stories</a></li>
-                <li><a href="addStory.php"><i class="fa fa-plus-circle"></i> Add Story</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
+    <!-- Static navbar -->
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/agnewsdb/index.php">AgNews DB</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <?php if(!empty($_SESSION["waitingOnWriter"])) : ?>
+              <li><a href="/agnewsdb/index.php"><span data-container="body" data-placement="bottom" rel='tooltip' title='You have <?php echo  $_SESSION["waitingOnWriter"];?> action(s) waiting' class='badge badge-info'><?php if($_SESSION["waitingOnWriter"] != 0) { echo $_SESSION["waitingOnWriter"]; } ?></span></a></li>
+            <?php endif; ?>
+              <li><a href="viewAllStories.php"><i class="fa fa-newspaper-o"></i> News Stories</a></li>
+              <li><a href="addStory.php"><i class="fa fa-plus-circle"></i> Add Story</a></li>
               <li><a href="help.php"><i class="fa fa-question-circle"></i> Help</a></li>
-              <?php if($_SESSION["isAdmin"]) : ?>
-                <li><a href="controlPanel.php"><i class="fa fa-cogs"></i> Control Panel</a></li>
-              <?php endif; ?>
-              <li><a href="?logout=1"><i class="fa fa-sign-out"></i> Logout</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div><!--/.container-fluid -->
-      </nav>
-
-
-
-
-
-
+              <li><a href="getNotified.php"><i class="fa fa-bell-o"></i> Get Notified</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <?php if($_SESSION["isAdmin"]) : ?>
+              <li><a href="controlPanel.php"><i class="fa fa-cogs"></i> Control Panel</a></li>
+            <?php endif; ?>
+            <li><a href="?logout=1"><i class="fa fa-sign-out"></i> Logout</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div><!--/.container-fluid -->
+    </nav>
--- index.php
+++ index.php
@@ -33,104 +33,7 @@ if ($_SESSION["success"] != "") {
 
 ?>
 
-
-      <?php
-/*
-        $today = strtotime('now');
-        $yesterday = strtotime('-1 day', $today);
-
-        echo "<h3>Recent Activity<span rel='tooltip' title='Shows activity in the last 48 hours made by others.'><i class='fa fa-question-circle'></i></span></h3>";
-        echo "<div class='row-fluid'>";
-
-
-        $sql = "SELECT * FROM tblActivity INNER JOIN tblNews ON tblNews.newsID=tblActivity.newsID WHERE tblActivity.peopleID <> " . $_SESSION["userID"] . " ORDER BY tblActivity.dateTimeStamp DESC LIMIT 3";
-        $result = mysql_query($sql);
-
-        while($newsActivityEach = mysql_fetch_array($result)) {
-          $activityDate = strtotime($newsActivityEach["dateTimeStamp"]) - $yesterday;
-          // post only if the activity is within the last 48 hours
-          if($activityDate <= 86400 && $activityDate > 0) {
-            echo "<div class='span4 well'>";
-            if(!empty($newsActivityEach["strHeadline"])) {
-              echo "<p><strong><a href='beholdStory.php?newsID=" . $newsActivityEach["newsID"] . "'>" . $newsActivityEach["strHeadline"] . "</a></strong><br /><small class='muted' rel='tooltip' style='cursor:pointer' title='Activity made on " .  $newsActivityEach["dateTimeStamp"] . "'>" . $newsActivityEach["strFirstName"] . " " . $newsActivityEach["strLastName"] .  "</small>";
-            }
-            else {
-              echo "<p><strong><a href='beholdStory.php?newsID=" . $newsActivityEach["newsID"] . "'>" . $newsActivityEach["strFilename"] . "</a></strong><br /><small class='muted' rel='tooltip' style='cursor:pointer' title='Activity made on " .  $newsActivityEach["dateTimeStamp"] . "'>" . $newsActivityEach["strFirstName"] . " " . $newsActivityEach["strLastName"] .  "</small>";
-            }
-            echo "<p>" . $newsActivityEach["strActivity"] . "</p>";
-            echo "<p><a class='btn btn-block btn-small' <a href='beholdStory.php?newsID=" . $newsActivityEach["newsID"] . "'>View Story <i class='icon-arrow-right'></i></a></p>";
-            echo "</div>";
-            $activityCount++;
-          }
-        }
-
-        if($activityCount == 0) {
-            echo "<div class='alert alert-info'>You have no recent activity.</div>";
-
-        }
-
-        echo "</div>";
-
-
-
-
-        $sql = "SELECT * FROM tblActivity INNER JOIN tblNews ON tblNews.newsID=tblActivity.newsID WHERE tblActivity.peopleID <> " . $_SESSION["userID"];
-        $result = mysql_query($sql);
-        $newsActivity = mysql_fetch_array($result);
-
-        // $sql = "SELECT * FROM tblNewsWriterPeople WHERE newsID=" . $newsActivity["newsID"] . " AND peopleID=" . $_SESSION["userID"];
-        // $result = mysql_query($sql);
-
-        $today = strtotime('now');
-        $yesterday = strtotime('-1 day', $today);
-
-
-
-      if((mysql_num_rows($result) > 0))   {
-         echo "<h3>Recent Activity<span rel='tooltip' title='Shows activity in the last 48 hours made by others.'><i class='icon-question-sign'></i></h3>";
-         echo "<div class='row-fluid'>";
-
-
-        $sql = "SELECT * FROM tblActivity INNER JOIN tblNews ON tblNews.newsID=tblActivity.newsID WHERE tblActivity.peopleID <> " . $_SESSION["userID"] . " ORDER BY tblActivity.dateTimeStamp DESC LIMIT 3";
-        $result = mysql_query($sql);
-
-        while($newsActivityEach = mysql_fetch_array($result)) {
-
-          $activityDate = strtotime($newsActivityEach["dateTimeStamp"]) - $yesterday;
-
-          // post only if the activity is within the last 48 hours
-          if($activityDate <= 86400 && $activityDate > 0) {
-            echo "<div class='span4 well'>";
-            if(!empty($newsActivityEach["strHeadline"])) {
-              echo "<p><strong><a href='beholdStory.php?newsID=" . $newsActivityEach["newsID"] . "'>" . $newsActivityEach["strHeadline"] . "</a></strong><br /><small class='muted' rel='tooltip' style='cursor:pointer' title='Activity made on " .  $newsActivityEach["dateTimeStamp"] . "'>" . $newsActivityEach["strFirstName"] . " " . $newsActivityEach["strLastName"] .  "</small>";
-            }
-            else {
-              echo "<p><strong><a href='beholdStory.php?newsID=" . $newsActivityEach["newsID"] . "'>" . $newsActivityEach["strFilename"] . "</a></strong><br /><small class='muted' rel='tooltip' style='cursor:pointer' title='Activity made on " .  $newsActivityEach["dateTimeStamp"] . "'>" . $newsActivityEach["strFirstName"] . " " . $newsActivityEach["strLastName"] .  "</small>";
-            }
-            echo "<p>" . $newsActivityEach["strActivity"] . "</p>";
-            echo "<p><a class='btn btn-block btn-small' <a href='beholdStory.php?newsID=" . $newsActivityEach["newsID"] . "'>View Story <i class='icon-arrow-right'></i></a></p>";
-            echo "</div>";
-            $activityCount++;
-          }
-        }
-
-        echo "</div>";
-
-
-      }
-      else {
-       echo "<div class='alert alert-info'>You have no recent activity.</div>";
-      }
-
-      */
-
-      ?>
-
-
-
       <h3>Your News Stories</h3>
-
-
       <?php
       // if there are news stories, post them in table format
       if($num_results > 0) {
@@ -140,29 +43,26 @@ if ($_SESSION["success"] != "") {
         }
       ?>
 
-
-
       <p>This section is populated with stories that involve you. <a href="viewAllStories.php">View all stories</a>.</p>
+      <div class="clearfix">
+        <a href="addStory.php" style="margin-left: 5px; margin-bottom:5px;" class="pull-right btn btn-default"><i class="fa fa-plus-circle"></i> Add Story</a>
 
-       <div class="clearfix">
-        <a href="addStory.php" style="margin-left: 5px; margin-bottom:5px;" class="pull-right btn btn-default">Add Story <i class="fa fa-plus-circle"></i></a>
-
-        <div class="form-group form-inline pull-right">
-          <form action="searchResults.php" method="get">
+        <form class="form-inline" action="searchResults.php" method="get">
+          <div class="form-group pull-right">
             <input class="form-control" id="appendedInputButtons" type="text" name="q" placeholder="Search Stories">
             <button class="btn btn-default" type="submit"><i class='fa fa-search' onClick="setConfirmUnload(false);"></i></button>
-          </form>
-        </div>
-        </div>
+          </div>
+        </form>
+      </div>
 
       <div class="panel panel-default"> <!-- for round corners -->
       <table class="table table-striped table-hover table-bordered">
       <tr>
-        <td align="center" width="3%"></td>
-        <td width="55%">Filename</td>
-        <td width="20%">Date Released</td>
-        <td width="20%">Status</td>
-        <td width="5%">Admin</td>
+        <td align="center" style="width: 3%"></td>
+        <td style="width: 45%">Filename</td>
+        <td style="width: 20%">Date Released</td>
+        <td style="width: 20%">Status</td>
+        <td style="width: 10%">Admin</td>
       </tr>
 
       <?php
@@ -170,20 +70,18 @@ if ($_SESSION["success"] != "") {
       // display stories that require their attention first
       while($redRow = mysql_fetch_array($redResult)) {
         // check date
-
-
         echo "<tr class='danger'>";
-        echo "<td align='center' width='3%''><i rel='tooltip' title='This story is waiting on the writer(s).' class='fa fa-minus-circle'></i></td>";
-        echo "<td width='55%'><a href='beholdStory.php?newsID=" . $redRow['newsID'] . "'>" . htmlspecialchars(stripslashes($redRow['strFilename']), ENT_QUOTES) . "</a></td>";
+        echo "<td align='center'><i rel='tooltip' title='This story is waiting on the writer(s).' class='fa fa-minus-circle'></i></td>";
+        echo "<td><a href='beholdStory.php?newsID=" . $redRow['newsID'] . "'>" . htmlspecialchars(stripslashes($redRow['strFilename']), ENT_QUOTES) . "</a></td>";
 
         if($redRow["datePublished"] != "0000-00-00") {
-          echo "<td width='15%'>" .  date("F d, Y", strtotime($redRow["datePublished"])) . "</td>";
+          echo "<td>" .  date("F d, Y", strtotime($redRow["datePublished"])) . "</td>";
         }
         else {
-          echo "<td width='15%'>TBD</td>";
+          echo "<td>TBD</td>";
         }
 
-        echo "<td width='20%'>";
+        echo "<td>";
         $sql2 = "SELECT strStatus FROM tblStatus WHERE statusID=" . $redRow['statusID'];
         $result2 = mysql_query($sql2);
         $row2 = mysql_fetch_array($result2);
@@ -193,7 +91,8 @@ if ($_SESSION["success"] != "") {
           echo "No status provided";
         }
         echo "</td>";
-        echo "<td align='center' width='5%''><a href='editStory.php?newsID=" . $redRow["newsID"] . "'' rel='tooltip' title='Edit " . $redRow['strFilename'] . "'> <i class='fa fa-edit'> </i></a> <a href='functions/doDeleteStory.php?newsID=" . $redRow["newsID"] . "' rel='tooltip' title='Remove " . $redRow['strFilename'] . "'><i class='fa fa-remove'></i></a></td>";
+        echo "<td align='center'><a class='btn btn-default btn-sm' href='editStory.php?newsID=" . $normalRow["newsID"] . "' rel='tooltip' title='Edit " . $normalRow['strFilename'] . "'><i class='fa fa-edit'></i></a> ";
+        echo "<a class='btn btn-default btn-sm' href='functions/doDeleteStory.php?newsID=" . $normalRow["newsID"] . "' rel='tooltip' title='Remove " . $normalRow['strFilename'] . "'><i class='fa fa-times'></i></a></td>";
         echo "</tr>";
       }
 
@@ -209,17 +108,17 @@ if ($_SESSION["success"] != "") {
 
 
         echo "<tr class='warning'>";
-        echo "<td align='center' width='3%''><i rel='tooltip' title='Story is waiting on approval from the coordinator, a source, or M&M.' class='fa fa-minus-circle'></i></td>";
-        echo "<td width='55%'><a href='beholdStory.php?newsID=" . $yellowRow['newsID'] . "'>" . $yellowRow['strFilename'] . "</a></td>";
+        echo "<td align='center'><i rel='tooltip' title='Story is waiting on approval from the coordinator, a source, or M&M.' class='fa fa-minus-circle'></i></td>";
+        echo "<td><a href='beholdStory.php?newsID=" . $yellowRow['newsID'] . "'>" . $yellowRow['strFilename'] . "</a></td>";
 
         if($yellowRow["datePublished"] != "0000-00-00") {
-          echo "<td width='15%'>" .  date("F d, Y", strtotime($yellowRow["datePublished"])) . "</td>";
+          echo "<td>" .  date("F d, Y", strtotime($yellowRow["datePublished"])) . "</td>";
         }
         else {
-          echo "<td width='15%'>TBD</td>";
+          echo "<td>TBD</td>";
         }
 
-        echo "<td width='20%'>";
+        echo "<td>";
         $sql2 = "SELECT strStatus FROM tblStatus WHERE statusID=" . $yellowRow['statusID'];
         $result2 = mysql_query($sql2);
         $row2 = mysql_fetch_array($result2);
@@ -229,13 +128,14 @@ if ($_SESSION["success"] != "") {
           echo "No status provided";
         }
         echo "</td>";
-        echo "<td align='center' width='5%''><a href='editStory.php?newsID=" . $yellowRow["newsID"] . "' rel='tooltip' title='Edit " . $yellowRow['strFilename'] . "'><i class='fa fa-edit'></i></a> <a href='functions/doDeleteStory.php?newsID=" . $yellowRow["newsID"] . "' rel='tooltip' title='Remove " . $yellowRow['strFilename'] . "'><i class='fa fa-remove'></i></a></td>";
+        echo "<td align='center'><a class='btn btn-default btn-sm' href='editStory.php?newsID=" . $normalRow["newsID"] . "' rel='tooltip' title='Edit " . $normalRow['strFilename'] . "'><i class='fa fa-edit'></i></a> ";
+        echo "<a class='btn btn-default btn-sm' href='functions/doDeleteStory.php?newsID=" . $normalRow["newsID"] . "' rel='tooltip' title='Remove " . $normalRow['strFilename'] . "'><i class='fa fa-times'></i></a></td>";
         echo "</tr>";
       }
 
       // finally, show their published stories
-      // TODO: limit #
       while($normalRow = mysql_fetch_array($normalResult)) {
+        if($limit < 5) {
         // check date
         $ts = strtotime($normalRow["datePublished"]);
         if ($ts === false || $ts == "1969-12-31")
@@ -244,17 +144,17 @@ if ($_SESSION["success"] != "") {
           $datePublished = date("m/d/y", $ts);
 
         echo "<tr>";
-        echo "<td align='center' width='3%''><i rel='tooltip' title='This story has been published successfully.' class='fa fa-check-circle-o'></i></td>";
-        echo "<td width='55%'><a href='beholdStory.php?newsID=" . $normalRow['newsID'] . "'>" . $normalRow['strFilename'] . "</a></td>";
+        echo "<td align='center'><i rel='tooltip' title='This story has been published successfully.' class='fa fa-check-circle-o'></i></td>";
+        echo "<td><a href='beholdStory.php?newsID=" . $normalRow['newsID'] . "'>" . $normalRow['strFilename'] . "</a></td>";
 
         if($normalRow["datePublished"] != "0000-00-00") {
-          echo "<td width='15%'>" .  date("F d, Y", strtotime($normalRow["datePublished"])) . "</td>";
+          echo "<td>" .  date("F d, Y", strtotime($normalRow["datePublished"])) . "</td>";
         }
         else {
-          echo "<td width='15%'>TBD</td>";
+          echo "<td>TBD</td>";
         }
 
-        echo "<td width='20%'>";
+        echo "<td>";
         $sql2 = "SELECT strStatus FROM tblStatus WHERE statusID=" . $normalRow['statusID'];
         $result2 = mysql_query($sql2);
         $row2 = mysql_fetch_array($result2);
@@ -264,16 +164,20 @@ if ($_SESSION["success"] != "") {
           echo "No status provided";
         }
         echo "</td>";
-        echo "<td align='center' width='5%''><a href='editStory.php?newsID=" . $normalRow["newsID"] . "' rel='tooltip' title='Edit " . $normalRow['strFilename'] . "'><i class='fa fa-edit'></i></a> <a href='functions/doDeleteStory.php?newsID=" . $normalRow["newsID"] . "' rel='tooltip' title='Remove " . $normalRow['strFilename'] . "'><i class='fa fa-times'></i></a></td>";
+        echo "<td align='center'><a class='btn btn-default btn-sm' href='editStory.php?newsID=" . $normalRow["newsID"] . "' rel='tooltip' title='Edit " . $normalRow['strFilename'] . "'><i class='fa fa-edit'></i></a> ";
+        echo "<a class='btn btn-default btn-sm' href='functions/doDeleteStory.php?newsID=" . $normalRow["newsID"] . "' rel='tooltip' title='Remove " . $normalRow['strFilename'] . "'><i class='fa fa-times'></i></a></td>";
+        echo "</tr>";
+        $limit++;
+      } //end limit if
+    } // end sql while
+      if($limit > 4) {
+        echo "<tr>";
+        echo "<td colspan='5'>";
+        echo "<a href='viewAllStories.php' class='btn btn-sm btn-block btn-link'><i class='fa fa-newspaper-o'></i> View All Stories</a>";
+        echo "</td>";
         echo "</tr>";
       }
-
-
-
-
-
         echo "</table>";
-
         echo "</div>";
       } // end if (regarding if there are news stories)
       else {
@@ -281,7 +185,6 @@ if ($_SESSION["success"] != "") {
       }
         ?>
 
-    <a href="getNotified.php"><img style="border:1px solid #dedede; width:100%; " src="img/signupalerts.jpg" alt="sign up for alerts" /></a>
 
     <?php
 // global includes
