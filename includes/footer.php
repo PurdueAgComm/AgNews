726e531024df3a2d81879e3431a1cb7b490a847c
Kenny Wilson <kenny@digital-inflection.com>
2015-07-10 17:11:46 -0400

    update changelog

--- includes/footer.php
+++ includes/footer.php
@@ -4,14 +4,17 @@
       <div class="footer" style="clear:both;">
         <!-- Site Launched January 2014 -->
         <p>&copy; Purdue University 2013-<?=date("Y")?> | You are logged in as <?php echo phpCAS::getAttribute('fullname');?>. <a href="?logout=1">Logout</a>
-        <span class="muted pull-right"><small><i class="fa fa-gift text-muted" data-container="body" rel="tooltip" title="Updated 2015!"></i> version 1.3</small></p>
+        <span class="muted pull-right"  data-container="body" rel="tooltip" title="Updated 2015"><small>version 2.0</small></p>
 
 
-      <div class="alert alert-info"><strong>1.3.0 Changelog</strong><br />
+      <div class="alert alert-info"><strong>2.0.0 Changelog</strong><br />
         <ul>
           <li>Changes to doUpdateStage.php</li>
           <li>Notables release</li>
-          <li>~ Bootstrap Upgrade</li>
+          <li>UI Updates</li>
+          <li>Columns New!</li>
+          <li>AgAnswers New!</li>
+          <li>Connections New!</li>
       </div>
 
     </div> <!-- /container -->
