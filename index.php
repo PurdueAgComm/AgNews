084ab44ec98eac0958055156f7a920807fd6c5ab
Kenny Wilson <kenny@digital-inflection.com>
2016-02-18 10:16:11 -0500

    changelog update officially closes #5

--- index.php
+++ index.php
@@ -40,27 +40,10 @@ if ($_SESSION["success"] != "") {
 //    // CHANGE COOKIE NAME TO THE NAME OF THE CURRENT VERSION FOR IT TO SHOW UP AGAIN
 // ************************************************************************************************************************************
 // ************************************************************************************************************************************
-if(!isset($_COOKIE['21'])) { ?>
-    <div class="panel panel-primary">
-      <div class="panel-heading">
-        <h3 class="panel-title">Changelog - v2.1</h3>
-      </div>
-      <div class="panel-body">
-          <p><label class="label label-success">Feature</label> <strong>Select to receive Extension Depot news in your email</strong>: For the administrators that need to receive the Extension Depot stories in their email (in order to add it manually to the Depot), they can now select to receive these updates in their user profile rather than coded in manually by MMU Web.</p>
-          <p><label class="label label-success">Feature</label> <strong>Extension Depot Manager Role</strong>: A user can now be assigned to the role of Extension Depot Manager. This user will receive updates when any user creates a new writer in order to manage the available writers in the Extension Depot</p>
-          <p><label class="label label-danger">Removed</label> <strong>Change Confirmation</strong>: Removed the change confirmation when navigating away from a page when a form input has been changed.</p>
-      </div>
-    </div>
-<?
-  // set a cookie to expire in a year
-  setcookie("21", "true", time()+60*60*24*365);
-}
-//END DISPLAY OF CHANGELOG
-
 if(!isset($_COOKIE['201'])) { ?>
     <div class="panel panel-primary">
       <div class="panel-heading">
-        <h3 class="panel-title">Changelog - v2.2</h3>
+        <h3 class="panel-title">Changelog - v2.0.1</h3>
       </div>
       <div class="panel-body">
           <p><label class="label label-success">Feature</label> <strong>Changelog Message</strong>: You'll receive a message outlining the latest changes to the AgNews Database the first time you login once an update has been released.</li></p>
@@ -72,13 +55,30 @@ if(!isset($_COOKIE['201'])) { ?>
   // set a cookie to expire in a year
   setcookie("201", "true", time()+60*60*24*365);
 }
+
+if(!isset($_COOKIE['210'])) { ?>
+    <div class="panel panel-primary">
+      <div class="panel-heading">
+        <h3 class="panel-title">Changelog - v2.1.0</h3>
+      </div>
+      <div class="panel-body">
+          <p><label class="label label-success">Feature</label> <strong>Select to receive Extension Depot news in your email</strong>: For the administrators that need to receive the Extension Depot stories in their email (in order to add it manually to the Depot), they can now select to receive these updates in their user profile rather than coded in manually by MMU Web.</p>
+          <p><label class="label label-success">Feature</label> <strong>Extension Depot Manager Role</strong>: A user can now be assigned to the role of Extension Depot Manager. This user will receive updates when any user creates a new writer in order to manage the available writers in the Extension Depot</p>
+          <p><label class="label label-danger">Removed</label> <strong>Change Confirmation</strong>: Removed the change confirmation when navigating away from a page when a form input has been changed.</p>
+      </div>
+    </div>
+<?
+  // set a cookie to expire in a year
+  setcookie("210", "true", time()+60*60*24*365);
+}
+
 //END DISPLAY OF CHANGELOG
 ?>
 
 <!-- What's currently in development and hasn't launched -->
     <div class="panel panel-primary">
       <div class="panel-heading">
-        <h3 class="panel-title">Changelog - v2.1.0</h3>
+        <h3 class="panel-title">Changelog - v2.2.0</h3>
       </div>
       <div class="panel-body">
           <p><label class="label label-success">Feature</label> <strong>Custom Reports</strong>: If you're an administrator, you're now able to create reports that fit your needs. Visit the control panel to make your custom report.</p>
